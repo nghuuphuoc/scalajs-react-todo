8938b4a36a7b5e1b90bccc16446c7444ff5383e1
Phuoc Nguyen <phuoc@huuphuoc.me>
2017-10-09 14:59:02 +0700

    Populate sample data

--- src/main/scala/foo/bar/todo/HomePage.scala
+++ src/main/scala/foo/bar/todo/HomePage.scala
@@ -14,15 +14,54 @@ object HomePage {
 
   private final val ComponentName = getClass.getSimpleName
 
-  private case class Backend(scope: BackendScope[HomePage, _]) {
+  private case class State(tasks: List[(Int, String, Boolean)])
 
-    def render(): TagOf[Div] = {
-      <.div("Homepage")
+  private case class Backend(scope: BackendScope[HomePage, State]) {
+
+    def render(state: State): TagOf[Div] = {
+      <.div(
+        <.ul(^.cls := "list pl0 mb2",
+          state.tasks.toTagMod { case (id, title, done) =>
+            <.li(^.cls := "flex items-center mb2",
+              <.input(
+                ^.cls := "mr2",
+                ^.tpe := "checkbox",
+                ^.id := s"task-$id",
+                ^.checked := done
+              ),
+              <.label(
+                ^.cls := "lh-copy",
+                ^.htmlFor := s"task-$id",
+                title
+              )
+            )
+          }
+        ),
+
+        <.div(^.cls := "flex items-center",
+          <.input(
+            ^.cls := "ba b--black-20 pa2",
+            ^.tpe := "input"
+          ),
+          <.button(
+            ^.cls := "white bg-blue ba b--near-white pa2",
+            ^.tpe := "button",
+            "Add"
+          )
+        )
+      )
     }
   }
 
   private val component = ScalaComponent.builder[HomePage](ComponentName)
-    .stateless
+    .initialState(
+      State(tasks = List(
+        (1, "Task 1", true),
+        (2, "Task 2", false),
+        (3, "Task 3", false),
+        (4, "Task 4", true)
+      ))
+    )
     .renderBackend[Backend]
     .build
 }
